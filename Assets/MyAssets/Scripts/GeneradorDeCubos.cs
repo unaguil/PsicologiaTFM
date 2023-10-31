5e45dab373be02c6d973295b88764866cfd0136b
Marian Angulo <marian.angulo@opendeusto.es>
2023-10-31 16:24:31 +0100

    Ajuste de tamaños

--- Assets/MyAssets/Scripts/GeneradorDeCubos.cs
+++ Assets/MyAssets/Scripts/GeneradorDeCubos.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class GeneradorDeCubos : MonoBehaviour
 {
     public GameObject cuboPrefab;
-    public static int contador = 0;
+    public static int contadorRojo = 0;
+    public static int numBolas = 0;
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.layer == LayerMask.NameToLayer("Cubo"))
         {
-            contador++;
-            Debug.Log("Canasta anotada. Contador: " + contador);
+            contadorRojo++;
+            Debug.Log("Canasta anotada. Contador: " + contadorRojo);
             Destroy(collision.gameObject);
             GenerarNuevoCubo();
+        }
+        else if (collision.gameObject.layer == LayerMask.NameToLayer("Bola"))
+        {
+            numBolas++;
+            Debug.Log("Bola: " + numBolas);
+            Destroy(collision.gameObject);
         }
 
     }
