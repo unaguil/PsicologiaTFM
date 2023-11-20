2d7777077dee1d36954fc9ced51d2189c9100c8a
Marian Angulo <marian.angulo@opendeusto.es>
2023-11-20 16:36:08 +0100

    Atajo y ajustes
    Co-Authored-By: Beñat Ramirez Ortiz <125921371+benramort@users.noreply.github.com>

--- Assets/MyAssets/Scripts/CambiarEscena.cs
+++ Assets/MyAssets/Scripts/CambiarEscena.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
 using UnityEngine.SceneManagement;
 
 public class CambiarEscena : MonoBehaviour
 {
+    public XRController controller = null;
+    InputDevice device;
     // Start is called before the first frame update
     public static IEnumerator EsperarYCambiarDeEscena(string nombreEscena)
     {
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(3);
         SceneManager.LoadScene(nombreEscena);
+    }
+    
+    public void saltarEscena()
+    {
+        Debug.Log("Saltar escena");
+        SceneManager.LoadScene("Situacion1");
     }
 }
