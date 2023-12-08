b740ab6124d20230010a326c1d1e5fbc85cf3cc3
Marian Angulo <marian.angulo@opendeusto.es>
2023-12-08 19:53:42 +0100

    Final

---
+++ Assets/MyAssets/Scripts/General/CerrarAplicación.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CerrarAplicación : MonoBehaviour
+{
+    public void CloseApp()
+    {
+        Application.Quit();
+    }
+}
