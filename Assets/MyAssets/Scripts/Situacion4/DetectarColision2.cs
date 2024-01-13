6229b4b158a196fcf470a32b62b267d6bdda65ed
Marian Angulo <marian.angulo@opendeusto.es>
2024-01-13 13:46:57 +0100

    Cambios finales 1
    Co-Authored-By: Beñat Ramirez Ortiz <125921371+benramort@users.noreply.github.com>

--- Assets/MyAssets/Scripts/Situacion4/DetectarColision2.cs
+++ Assets/MyAssets/Scripts/Situacion4/DetectarColision2.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DetectarColision2 : MonoBehaviour
 {
-    private Vector3 posicion = new Vector3(1.119f, 0.263f, -3.64f);
+    private Vector3 posicion = new Vector3(1.119f, 0.35f, -4.48f);
     public GameObject red;
 
     public void OnCollisionEnter(Collision collision)
