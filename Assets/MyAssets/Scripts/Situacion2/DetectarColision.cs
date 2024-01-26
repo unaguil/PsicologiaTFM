3d021600a9d784b58b8ae584cfbd25db6ac073a0
Marian Angulo <marian.angulo@opendeusto.es>
2024-01-26 11:52:12 +0100

    Cambios post-prueba

--- Assets/MyAssets/Scripts/Situacion2/DetectarColision.cs
+++ Assets/MyAssets/Scripts/Situacion2/DetectarColision.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class DetectarColision : MonoBehaviour
 {
-    private Vector3 posicion = new Vector3(0.999995232f, 0f, -3.21700168f);
+    private Vector3 posicion = new Vector3(-2.3f, 1f, 2.6f);
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     public void OnCollisionEnter(Collision collision)
     {
         Debug.Log(collision.gameObject.name);
-        if (collision.gameObject.name == "ParteRestaurante")
+        if (collision.gameObject.name == "collider bolas")
         {
             Debug.Log("Llega");
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
             gameObject.transform.position = posicion;
         }
     }
